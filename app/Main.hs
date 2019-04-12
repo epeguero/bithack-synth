8c6b1bd4587468cce1dfe108db1e0588487ea249
Edwin Peguero <emp238@cornell.edu>
2019-04-12 00:32:00 -0400

    implemented parsers for bitvector terms, propositions, and have a working (but incomplete) sketch parser.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE RecursiveDo #-} 
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -6,40 +8,82 @@ import System.Environment
 import Data.Char
 import Text.Earley
 import Control.Applicative
+import Control.Monad.ST
 import Data.Functor (($>))
 
+----- Sketch -----
+data Sketch = Term BitVecTerm | Prop BitVecProp deriving Show
+sketchGrammar :: forall r. Grammar r (Prod r () Char Sketch)
+sketchGrammar =  (\bv zolBv -> Term <$> bv <|> Prop <$> zolBv) <$> bvTermGrammar <*> bvPropGrammar 
+
+sketchParser :: String -> ([Sketch], Report () String)
+sketchParser = generateParser sketchGrammar
+ 
+----- Bit Vector Proposition -----
+data BitVecProp = BvPropApp BitVecTerm BvProp BitVecTerm
+  deriving Show
+data BvProp = BvEq | BvNe | BvLt | BvGt | BvLe | BvGe
+  deriving Show
+
+bvPropGrammar :: forall r. Grammar r (Prod r () Char BitVecProp)
+bvPropGrammar = (\bv -> BvPropApp <$> bv <*> bv_prop <*> bv) <$> bvTermGrammar
+  where bv_prop =     BvEq <$ string "=_bv" <|> BvNe <$ string "/=_bv" 
+                  <|> BvGt <$ string ">" <|> BvGe <$ string ">=" 
+                  <|> BvLt <$ string "<" <|> BvLe <$ string "<="
+
+bvPropParser :: String -> ([BitVecProp], Report () String)
+bvPropParser = generateParser bvPropGrammar
+
+----- Bit Vector Term -----
+data BitVecTerm = Hole | BitVecVal Int | Var String | BvFunApp BitVecTerm BvFun BitVecTerm
+  deriving Show
+data BvFun = Bwand | Lshift | Rshift | Plus | Minus
+  deriving Show
+
+bvTermGrammar :: forall r. Grammar r (Prod r () Char BitVecTerm)
+bvTermGrammar = mdo
+  bv1 <- rule $ (BvFunApp <$> bv1) <*> bv_op1 <*> bv1 <|> bv2
+  bv2 <- rule $ (BvFunApp <$> bv2) <*> bv_op2 <*> bv2 <|> bv3
+  bv3 <- rule $ (BvFunApp <$> bv3) <*> bv_op3 <*> bv3 <|> base
+
+  bv_op1 <- rule $ Bwand <$ string "&"
+  bv_op2 <- rule $ Lshift <$ string "<<" <|> Rshift <$ string ">>"
+  bv_op3 <- rule $ Plus <$ string "+" <|> Minus <$ string "-"
+
+  base <- rule $ Hole <$ string "??" <|> BitVecVal <$> number <|> Var <$> var
+
+  return bv1
+
+bvTermParser :: String -> ([BitVecTerm], Report () String)
+bvTermParser = generateParser bvTermGrammar
+
+----- Low Level Parsers -----
+ws = many $ satisfy isSpace :: Prod r e Char String
+leadingWs p = ws *> p 
+
+string s = leadingWs $ list s :: Prod r e Char String
+
+number :: Prod r e Char Int
+number = leadingWs $ (read :: String -> Int) <$> (some . satisfy $ isDigit)
+
+var = leadingWs $ some $ satisfy isAlpha :: Prod r e Char String
+
+
+----- User / Debugging Interface -----
 main :: IO()
 main = do
   x:_ <- getArgs
-  print $ parse x 
+  printParses $ bvTermParser x
 
-parse = fullParses (parser bv)
+printParses :: forall e a. (Show e, Show a) => ([a], Report e String) -> IO ()
+printParses = print
 
-data BitVecTerm = Hole | BitVecVal Int | Var String | App BitVecTerm Fun BitVecTerm
-  deriving Show
-data Fun = Bwand | Lshift | Rshift | Plus | Minus
-  deriving Show
+generateParser :: forall e a. 
+          (forall r. Grammar r (Prod r e Char a)) -> 
+          String -> ([a], Report e String)
+generateParser g = f
+  where f :: String -> ([a], Report e String)
+        f = fullParses p
 
-bv :: Grammar r (Prod r () Char BitVecTerm)
-bv = mdo
-  bv1 <- rule $ (App <$> bv1) <*> bv_op1 <*> bv1 <|> bv2
-  bv2 <- rule $ (App <$> bv2) <*> bv_op2 <*> bv2 <|> bv3
-  bv3 <- rule $ (App <$> bv3) <*> bv_op3 <*> bv3 <|> base
-
-  bv_op1 <- rule $ Bwand <$ token "&"
-  bv_op2 <- rule $ Lshift <$ token "<<" <|> Rshift <$ token ">>"
-  bv_op3 <- rule $ Plus <$ token "+" <|> Minus <$ token "-"
-
-  base <- rule $ Hole <$ token "??" <|> BitVecVal <$> number <|> Var <$> var
-
-  return bv1
-  where
-    ws = many $ satisfy isSpace :: Prod r e Char String
-    leading_ws p = ws *> p 
-
-    token s = leading_ws $ list s :: Prod r e Char String
-
-    number :: Prod r e Char Int
-    number = leading_ws $ (read :: String -> Int) <$> (some . satisfy $ isDigit)
-
-    var = leading_ws $ some $ satisfy isAlpha :: Prod r e Char String
+        p :: Parser e String a
+        p = parser g
