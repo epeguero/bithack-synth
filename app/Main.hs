71fa7a26339e87bcd4bda9fc392466343f17c95d
Edwin Peguero <emp238@cornell.edu>
2019-04-14 16:35:48 -0400

    type family implementation of basic theory and sketch grammar.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE RecursiveDo #-} 
-{-# LANGUAGE Rank2Types #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE Rank2Types, ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies, StandaloneDeriving, UndecidableInstances #-}
 
 module Main where
 
@@ -16,9 +13,48 @@ import Data.Foldable (asum)
 type Gram r e ast = Grammar r (Prod r e Char ast)
 type ParserOutput e ast = ([ast], Report e String)
 
+main = print "Hey"
+
+type family Value t
+type family FunF t 
+type family FunP t
+
+data Formula t = Val (Value t) | AppF (FunF t) [Formula t]
+deriving instance (Show (Value t), Show (FunF t)) => Show (Formula t)
+data Prop t = AppP (FunP t) [Formula t]
+deriving instance (Show (Value t), Show (FunP t), Show (FunF t)) => Show (Prop t)
+
+-- Defining sketches orthogonally from theory
+data ValSketch :: * -> *
+type instance Value (ValSketch t) = Maybe (Value t)
+type instance FunF (ValSketch t) = FunF t
+type instance FunP (ValSketch t) = FunP t
+
+type instance Value (Either t1 t2) = Either (Value t1) (Value t2)
+type instance FunF (Either t1 t2) = Either (Value t1) (Value t2)
+
+type Expr = Formula (ValSketch BitVectorTheory) -- (Logic FirstOrder (Theory BitVector))
+
+
+-- Bit Vector Theory instantiation
+data BitVectorTheory 
+type instance Value BitVectorTheory = Int
+type instance FunF BitVectorTheory = BitVectorFunctions
+type instance FunP BitVectorTheory = BitVectorPropositions
+data BitVectorFunctions = Lshift | Rshift
+  deriving Show
+data BitVectorPropositions = BvEq | BvLt
+  deriving Show
+
+
+
+
+
+
+{-
 ----- Supported Theories -----
-data Term t = VarT String | ValT (Value t) | BinT (Term t) (FunT t) (Term t)
-data Prop t = BinP (Term t) (FunP t) (Term t)
+data Term t = VarT String | ValT (Value t) | AppT (FunT t) [(Term t)]
+data Prop t = AppP (FunP t) [(Term t)]
 
 class (Show (FunT t), Show (FunP t)) => SupportedTheory t where
   data Value t :: *
@@ -34,7 +70,7 @@ class (Show (FunT t), Show (FunP t)) => SupportedTheory t where
     where 
       termPrec :: [FunT t] -> Prod r e Char (Term t) -> Gram r e (Term t)
       termPrec ops next = mdo  
-        t <- rule $ BinT <$> t <*> op <*> t <|> next
+        t <- rule $ buildAppT <$> t <*> op <*> t <|> next
         op <- rule $ 
               asum --foldr (<|>) empty 
               . map (\op -> op <$ (string . show $ op))
@@ -43,6 +79,9 @@ class (Show (FunT t), Show (FunP t)) => SupportedTheory t where
 
       base :: Gram r e (Term t)
       base = rule $ ValT <$> value <|> VarT <$> var
+ 
+      buildAppT :: Term t -> FunT t -> Term t -> Term t
+      buildAppT t1 op t2 = AppT op [t1, t2]
 
   termParser :: String -> ParserOutput e (Term t)
   termParser = fullParses p
@@ -56,19 +95,21 @@ class (Show (FunT t), Show (FunP t)) => SupportedTheory t where
     where 
       propPrec :: [FunP t] -> Prod r e Char (Prop t) -> Gram r e (Prop t)
       propPrec ops next = (\t -> mdo  
-        p <- rule $ BinP <$> t <*> op <*> t <|> next
+        p <- rule $ buildProp <$> t <*> op <*> t <|> next
         op <- rule $ 
-              asum --foldr (<|>) empty 
+              asum 
               . map (\op -> op <$ (string . show $ op))
               $ ops
         return p) =<< term
+  
+      buildProp :: Term t -> FunP t -> Term t -> Prop t
+      buildProp t1 op t2 = AppP op [t1, t2]
 
   propParser :: String -> ParserOutput e (Prop t)
   propParser = fullParses p
     where
       p :: Parser e String (Prop t)
       p = parser prop
-
 
 
 data BitVectorTheory
@@ -83,6 +124,7 @@ instance SupportedTheory BitVectorTheory where
   funTPrec = [[Bwand], [Lshift, Rshift], [Plus, Minus]]
   funPPrec = [[BvEq, BvNe], [BvGt, BvLt, BvGe, BvLe]]
 
+
 instance Show (FunT BitVectorTheory) where
   show Bwand = "&"
   show Lshift = "<<"
@@ -91,77 +133,32 @@ instance Show (FunT BitVectorTheory) where
   show Minus = "-"
 
 instance Show (FunP BitVectorTheory) where
-  show BvEq = "=_bv"
+  show BvEq = "==_bv"
   show BvNe = "/=_bv"
   show BvLt = "<"
   show BvGt = ">"
   show BvLe = "<="
   show BvGe = ">="
 
-
 instance Show (Term BitVectorTheory) where
   show (VarT x) = show x
   show (ValT v) = show v
-  show (BinT t1 op t2) = "( " ++ show t1 ++ " " ++ show op ++ " " ++ show t2 ++ " )"
+  show (AppT op [t]) = "( " ++ show op ++ show t ++ " )"
+  show (AppT op [t1, t2]) = "( " ++ show t1 ++ " " ++ show op 
+                                 ++ " " ++ show t2 ++ " )"
 
 instance Show (Prop BitVectorTheory) where
-  show (BinP t1 op t2) = "( " ++ show t1 ++ " " ++ show op ++ " " ++ show t2 ++ " )"
+  show (AppP op [t1, t2]) = "( " ++ show t1 ++ " " ++ show op ++ " " ++ show t2 ++ " )"
 
-{-
 
------ Sketch -----
-data Sketch = TermBv BitVecTerm | PropBv BitVecProp deriving Show
-sketchGrammar :: Gram Sketch
-sketchGrammar =  (\bv zolBv -> TermBv <$> bv <|> PropBv <$> zolBv) 
-                  <$> bvTermGrammar <*> bvPropGrammar 
-
-sketchParser :: String -> ParserOutput () Sketch
-sketchParser = generateParser sketchGrammar
- 
-
------ Bit Vector Proposition -----
-data BitVecProp = BvPropApp BitVecTerm BvProp BitVecTerm
+data Logic [t] LOrder
+data LOrder = Zero | First
+data Logic [t] Zero = Boolean0 Bool | MT0 (Prop [t]) | AppL0 FunL [Logic [t] Zero]
   deriving Show
-data BvProp = BvEq | BvNe | BvLt | BvGt | BvLe | BvGe
+data Logic [t] First = Boolean1 Bool | MT1 (Prop [t]) | AppL1 FunL [Logic [t] Zero]
+                       Forall (Logic [t] First) | Exists (Logic [t] First) | 
+                       Let String (Logic [t] First) (Logic [t] First)
   deriving Show
-
-bvPropGrammar :: Gram BitVecProp
-bvPropGrammar = (\bv -> BvPropApp <$> bv <*> bv_prop <*> bv) <$> bvTermGrammar
-  where bv_prop =     BvEq <$ string "=_bv" <|> BvNe <$ string "/=_bv" 
-                  <|> BvGt <$ string ">" <|> BvGe <$ string ">=" 
-                  <|> BvLt <$ string "<" <|> BvLe <$ string "<="
-
-bvPropParser :: String -> ParserOutput () BitVecProp
-bvPropParser = generateParser bvPropGrammar
-
-
------ Bit Vector Term -----
-data BitVecTerm = Hole | BitVecVal Int | Var String | BvFunApp BitVecTerm BvFun BitVecTerm
-  deriving Show
-data BvFun = Bwand | Lshift | Rshift | Plus | Minus
-  deriving Show
-
-bvTermGrammar :: Gram BitVecTerm
-bvTermGrammar = mdo
-  bv1 <- rule $ (BvFunApp <$> bv1) <*> bv_op1 <*> bv1 <|> bv2
-
-  bv_op1 <- rule $ Bwand <$ string "&"
-  bv_op2 <- rule $ Lshift <$ string "<<" <|> Rshift <$ string ">>"
-  bv_op3 <- rule $ Plus <$ string "+" <|> Minus <$ string "-"
-
-  base <- rule $ Hole <$ string "??" <|> BitVecVal <$> number <|> Var <$> var
-
-  return bv1
-
-bvTermParser :: String -> ParserOutput () BitVecTerm
-bvTermParser = generateParser bvTermGrammar
-
-
------ Zero-order logic -----
--- data ZOL t = BoolZol Bool | Not (Zol t) | BinPropZol (ZOL t) BinOpZol (Zol t) | TheoryProp t
--- data BinOpZol = AndZol | OrZol | ImpliesZol
-
-
 -}
 ----- Low Level Parsers -----
 ws = many $ satisfy isSpace :: Prod r e Char String
@@ -175,6 +172,7 @@ number = leadingWs $ (read :: String -> Int) <$> (some . satisfy $ isDigit)
 var = leadingWs $ some $ satisfy isAlpha :: Prod r e Char String
 
 
+{-
 ----- User / Debugging Interface -----
 main :: IO()
 main = do
@@ -186,3 +184,4 @@ bvTerm s = print (termParser s :: ParserOutput () (Term BitVectorTheory))
 
 bvProp:: String -> IO ()
 bvProp s = print (propParser s :: ParserOutput () (Prop BitVectorTheory))
+-}
